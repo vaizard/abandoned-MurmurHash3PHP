8ff7c3b067de3a04b08b85ab6e4bf6b2d48c5f2e
Matthew Crenshaw <sgt@squig.gs>
2014-01-14 17:14:36 -0500

    Created new 128 bit and 32 bit hasing functions in php. Aliased original function to the new 128 bit function.

--- php_murmurhash3.h
+++ php_murmurhash3.h
@@ -20,10 +20,11 @@
 #ifndef PHP_MURMURHASH3_H
   #define PHP_MURMURHASH3_H 1
 
-  #define PHP_MURMURHASH3_VERSION "0.1"
+  #define PHP_MURMURHASH3_VERSION "0.3"
   #define PHP_MURMURHASH3_EXTNAME "murmurhash3"
 
-  PHP_FUNCTION(murmurhash3);
+  ZEND_FUNCTION(murmurhash3_128);
+  ZEND_FUNCTION(murmurhash3_32);
 
   extern zend_module_entry murmurhash3_module_entry;
   #define phpext_murmurhash3_ptr &murmurhash3_module_entry
